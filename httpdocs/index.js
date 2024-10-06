b70a21a6200db839b94226252e68b21d5575b18f
BlockyDotJar <hallo.kevinand@gmail.com>
2024-10-06 21:20:54 +0200

    Remove every ApuJar API endpoint and update dependencies!

--- httpdocs/index.js
+++ httpdocs/index.js
@@ -12,10 +12,6 @@ const base = require("./routes/base");
 const regex = require("./routes/v1/regex");
 const flags = require("./routes/v1/flags");
 const links = require("./routes/v1/links");
-const user = require("./routes/v1/apujar/internal/user");
-const admins = require("./routes/v1/apujar/admins");
-const owners = require("./routes/v1/apujar/owners");
-const bible = require("./routes/v1/apujar/bible");
 
 app.use(express.json());
 app.use(cors());
@@ -36,10 +32,6 @@ base.setup(app);
 regex.setup(app);
 flags.setup(app);
 links.setup(app);
-user.setup(app);
-admins.setup(app);
-owners.setup(app);
-bible.setup(app);
 
 const file = fs.readFileSync("./httpdocs/swagger.yaml", "UTF8");
 const swaggerDocument = yaml.parse(file);
--- httpdocs/utils/mysql.js
+++ httpdocs/utils/mysql.js
@@ -44,71 +44,6 @@ const requestDatabase = (connection, query, values, res) =>
  * Response related database utility functions
  */
 
-async function getAllAdmins(res, connection, excludeOwner)
-{
-    let query = "SELECT * FROM `admins`";
-    let values = [];
-
-    if (excludeOwner)
-    {
-        query += " WHERE `isOwner` = ?"
-        values = [ false ];
-    }
-
-    const [ results ] = await requestDatabase(connection, query, values, res);
-    const admins = results[0];
-
-    return admins;
-}
-
-async function isAdmin(res, connection, userID)
-{
-    const admins = await getAllAdmins(res, connection);
-
-    const isAdmin = admins.some(admin =>
-    {
-        const adminID = admin.userID;
-        return adminID === userID;
-    });
-
-    return isAdmin;
-}
-
-async function getAllOwners(res, connection)
-{
-    const query = "SELECT * FROM `admins` WHERE `isOwner` = ?";
-    const values = [ true ];
-
-    const [ results ] = await requestDatabase(connection, query, values, res);
-    const owners = results[0];
-
-    return owners;
-}
-
-async function isOwner(res, connection, userID)
-{
-    const owners = await getAllOwners(res, connection);
-
-    const isOwner = owners.some(owner =>
-    {
-        const ownerID = owner.userID;
-        return ownerID === userID;
-    });
-
-    return isOwner;
-}
-
-async function getAllBibleEntries(res, connection)
-{
-    const query = "SELECT * FROM `bible`";
-    const values = [];
-
-    const [ results ] = await requestDatabase(connection, query, values, res);
-    const bibleEntries = results[0];
-
-    return bibleEntries;
-}
-
 async function getAllLinks(res, connection)
 {
     const query = "SELECT * FROM `links`";
@@ -141,11 +76,6 @@ module.exports =
 {
     createDatabaseConnection: createDatabaseConnection,
     requestDatabase: requestDatabase,
-    getAllAdmins: getAllAdmins,
-    isAdmin: isAdmin,
-    getAllOwners: getAllOwners,
-    isOwner: isOwner,
-    getAllBibleEntries: getAllBibleEntries,
     getAllLinks: getAllLinks,
     isValidUUID:  isValidUUID
 };
